7695778b614c79fcea181d2438b5b942f34b8014
shvvkz <viggocasciano0301@gmail.com>
2025-01-30 14:32:25 +0100

    lexer done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+pub mod lex;
+
+use lex::{lexer::Lexer, models::token_type::TokenType};
+
 fn main() {
-    println!("Hello, world!");
+    //let input = String::from("function calc(x: float, y: char){\nlet x = 5.0;\nlet y = 'z';\nlet result = x + y;\nreturn result;\n}");
+    let input = std::fs::read_to_string("test.own").unwrap();
+    println!("{}", input);
+    let mut lexer = Lexer::new(input);
+
+    loop {
+        let token = lexer.next_token();
+        if token.token_type == TokenType::EOF {
+            break;
+        }
+        println!("{:?}", token);
+    }
+    
 }
